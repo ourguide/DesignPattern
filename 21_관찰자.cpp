615c34cf953005aff2702dbaef07002e2f03af8e
Chansik Yun <chansigi@gmail.com>
2020-02-19 10:11:37 +0900

    .

--- 21_관찰자.cpp
+++ 21_관찰자.cpp
@@ -1,7 +1,11 @@
 // 21_관찰자
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+
+#include "ioacademy.h"
+using namespace ioacademy;
 
 // 1. 개념: 하나의 이벤트를 여러 객체에 전달
 // 2. Subject의 data를 전달하는 방식
@@ -22,10 +26,36 @@ class Subject
 {
 	vector<IObserver*> v;
 public:
+	Subject()
+	{
+		// 약속된 디렉토리의 모든 DLL을 열거한다.
+		IoEnumFiles("D:\\Plugin", "*.dll", LoadModule, this);
+	}
+
+	static int LoadModule(string name, void* param)
+	{
+		cout << name << endl;
+		// 1. DLL Load
+		void* addr = IoLoadModule(name);
+
+		// 2. 약속된 함수 CreateGraph를 찾습니다.
+		// IObserver* CreateGraph()
+		typedef IObserver* (*FP)();
+
+		FP f = (FP)IoGetFunctionAddress(addr, "CreateGraph");
+
+		// 3. 그래프 생성
+		IObserver* p = f();
+		Subject* self = static_cast<Subject*>(param);
+		self->Attach(p);
+
+		return 1;
+	}
+
+
 	void Attach(IObserver* p) { v.push_back(p); }
 	void Detach(IObserver* p) {}
 
-	// !!
 	void Notify(void *data)
 	{
 		for (int i = 0; i < v.size(); ++i)
